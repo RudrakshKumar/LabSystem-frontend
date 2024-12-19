52e99ddc23a8a16480cc6259c16751fcf9f27593
tus@19790 <tushar3145@gmail.com>
2024-12-19 10:16:20 +0530

    integrate backend

--- src/api.js
+++ src/api.js
@@ -2,19 +2,27 @@ import axios from "axios";
 import { LANGUAGE_VERSIONS } from "./constants";
 
 const API = axios.create({
-  baseURL: "https://emkc.org/api/v2/piston",
+  baseURL: "http://localhost:8080", // Base URL without `/execute`
 });
 
 export const executeCode = async (language, sourceCode) => {
-  console.log(sourceCode);
-  const response = await API.post("/execute", {
+  //console.log(sourceCode);
+
+  // Preparing the payload according to the curl format
+  const payload = {
     language: language,
-    version: LANGUAGE_VERSIONS[language],
-    files: [
-      {
-        content: sourceCode,
-      },
-    ],
+    code: sourceCode,  // Only `code` is required, not `files`
+  };
+
+  // Making the POST request to the `/execute` endpoint
+  const response = await API.post("/execute", payload, {
+    headers: {
+      "Content-Type": "application/json", // Ensuring content type is JSON
+    },
   });
-  return response.data;
+
+  return {
+    output: response.data.output.split("\n"), // Split output by newline
+    stderr: response.data.error, // Handle any errors (stderr)
+  };
 };
